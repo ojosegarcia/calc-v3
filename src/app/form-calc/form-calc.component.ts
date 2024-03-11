e0abd8f321fd240e415b9c6ea616128a838a3003
Jose Garcia <josecarlosgarciajunior2003@gmail.com>
2024-03-11 17:43:07 -0300

    subtrair

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -3,14 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-form-calc',
   templateUrl: './form-calc.component.html',
-  styleUrl: './form-calc.component.css'
+  styleUrls: ['./form-calc.component.css']
 })
 export class FormCalcComponent {
-  num1: number =0;
-  num2:number =0;
-  sum: number =0;
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
+  operation: string = '';
 
-  onClickSum(){
-    this.sum = this.num1 + this.num2;
+  onClickCalculate(operation: string) {
+    switch (operation) {
+      case 'somar':
+        this.result = this.num1 + this.num2;
+        break;
+      case 'subtrai':
+        this.result = this.num1 - this.num2;
+        break;
+      case 'multiply':
+        this.result = this.num1 * this.num2;
+        break;
+      case 'divide':
+        this.result = this.num1 / this.num2;
+        break;
+      case 'exponent':
+        this.result = Math.pow(this.num1, this.num2);
+        break;
+      case 'sqrt':
+        this.result = Math.sqrt(this.num1);
+        break;
+      case 'percentage':
+        this.result = (this.num1 * this.num2) / 100;
+        break;
+      default:
+        this.result = 0;
+    }
+    this.operation = operation;
   }
 }
